1600b6ea17945cddefd379ccdc9dc3ea65b7182c
jerrymusaga <musagatechnology@gmail.com>
2022-08-09 14:06:49 +0100

    chore:added more css to index.css

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {FaTrashAlt} from 'react-icons/fa';
 
 const Content = () => {
-    const handleChange = () => {
-        const names = ["Jerry", "Beatrice", "Lovers"];
-        let int = Math.floor(Math.random() * 3);
-        return names[int];
-    }
+    const [items, setItems] = useState([
+        {
+            'id': 1,
+            'item': "This is the first item",
+            'checked': false
+        },
+        {
+            'id': 2,
+            'item': "This is the second item",
+            'checked': true
+        },
+        {
+            'id': 3,
+            'item': "This is the third item",
+            'checked': true 
+        }
+    ]);
+
     return (
         <main>
-            <p>
-                Hi {handleChange()}
-            </p>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id} className='item'>
+                        <input
+                            type="checkbox"
+                            checked={item.checked}
+                        />
+                        <label>
+                            {item.item}
+                        </label>
+                        <FaTrashAlt 
+                            role="button" 
+                            tabIndex="0" 
+                        />
+                    </li>
+                ))}
+            </ul>
         </main>
+
     )
 }
 
