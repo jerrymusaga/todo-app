4bc6b2f92b89bcc9f859daabbecc2bc57d88a644
jerrymusaga <musagatechnology@gmail.com>
2022-08-12 02:29:37 +0100

    feature: added a create a new item feature

---
+++ src/AddItem.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa'
+
+const AddItem = ({newItem, setNewItem, handleNewItemSubmit}) => {
+  return (
+    <form className='addForm' onSubmit={handleNewItemSubmit}>
+        <label htmlFor='addItem'>Add Item</label>
+        <input 
+            onChange={(e) => setNewItem(e.target.value)}
+            autoFocus
+            id='addItem'
+            type='text'
+            placeholder='Add Item'
+            value={newItem}
+            required
+        />
+        <button
+            type='submit'
+            aria-label='Add Item'
+        >
+            <FaPlus />
+        </button>
+    </form>
+  )
+}
+
+export default AddItem
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Header } from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import AddItem from './AddItem';
 
 function App() {
 
@@ -21,23 +22,46 @@ function App() {
         'item': "This is the third item",
         'checked': true 
     }
-]);
+  ]);
 
-const handleCheck = (id) => {
-    const listItem = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
+  const [newItem, setNewItem] = useState('');
+
+  const addNewItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const newItem = {id, item, checked: false}
+    const listItem = [...items, newItem]
+    submitAndsave(listItem);
+  }
+
+  const handleNewItemSubmit = (e) => {
+    e.preventDefault()
+    addNewItem(newItem)
+    setNewItem('')
+  }
+
+  const submitAndsave = (listItem) => {
     setItems(listItem)
     localStorage.setItem('shoppingList', JSON.stringify(listItem));
-}
+  }
 
-const handleDelete = (id) => {
-    const listItem = items.filter((item) => item.id !== id )
-    setItems(listItem)
-    localStorage.setItem('shoppingList', JSON.stringify(listItem));
-}
+  const handleCheck = (id) => {
+      const listItem = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
+      submitAndsave(listItem);
+  }
+
+  const handleDelete = (id) => {
+      const listItem = items.filter((item) => item.id !== id )
+      submitAndsave(listItem)
+  }
   
   return (
     <div className="App">
       <Header title="My Todo-List"/>
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleNewItemSubmit={handleNewItemSubmit}
+      />
       <Content 
         items={items}
         handleCheck={handleCheck}
