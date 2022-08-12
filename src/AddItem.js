9b3ca65912af4999e3b0fb643a3ac40a074109e8
jerrymusaga <musagatechnology@gmail.com>
2022-08-12 03:58:38 +0100

    chore:using ref to switch focus to input field

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { FaPlus } from 'react-icons/fa'
 
 const AddItem = ({newItem, setNewItem, handleNewItemSubmit}) => {
-  
+  const inputRef = useRef();
   return (
     <form className='addForm' onSubmit={handleNewItemSubmit}>
         <label htmlFor='addItem'>Add Item</label>
         <input 
-            
+            ref={inputRef}
             onChange={(e) => setNewItem(e.target.value)}
             autoFocus
             id='addItem'
@@ -19,7 +19,7 @@ const AddItem = ({newItem, setNewItem, handleNewItemSubmit}) => {
         <button
             type='submit'
             aria-label='Add Item'
-            
+            onClick={() => inputRef.current.focus()}
         >
             <FaPlus />
         </button>
